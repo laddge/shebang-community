3a424e72a9ededf11950f3fd6231ec8550c314ff
Laddge <dev.laddge@gmail.com>
2022-04-07 11:50:33 +0000

    [fix] コード修正

--- discord_bot.py
+++ discord_bot.py
@@ -5,18 +5,18 @@ import discord
 client = discord.Client()
 
 
-def update_mc():
+async def update_mc():
     dtstr = datetime.datetime.now(
         datetime.timezone(datetime.timedelta(hours=9))
     ).strftime("%y-%m-%d %H:%M:%S")
-    guild = client.get_guild(os.getenv("GUILD_ID"))
-    member_role = guild.get_role(os.getenv("ROLE_ID"))
+    guild = client.get_guild(int(os.getenv("GUILD_ID")))
+    member_role = guild.get_role(int(os.getenv("ROLE_ID")))
     mc = sum(1 for member in member_role.members if not member.bot)
     print(f"[{dtstr}] {mc} members has member role")
-    channel = client.get_channel(os.getenv("CHANNEL_ID"))
+    channel = client.get_channel(int(os.getenv("CHANNEL_ID")))
     err = False
     try:
-        channel.edit(name="メンバー数: {mc}")
+        await channel.edit(name="メンバー数: {mc}")
     except Exception as e:
         print(e)
         err = True
@@ -25,17 +25,17 @@ def update_mc():
 
 @client.event
 async def on_raw_reaction_add(p):
-    update_mc()
+    await update_mc()
 
 
 @client.event
 async def on_raw_reaction_remove(p):
-    update_mc()
+    await update_mc()
 
 
 @client.event
 async def on_raw_reaction_clear_emoji(p):
-    update_mc()
+    await update_mc()
 
 
 @client.event
@@ -43,7 +43,7 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.content.strip() == "sb!count":
-        mc, err = update_mc()
+        mc, err = await update_mc()
         await message.channel.send(f"現在のメンバー数は {mc} です。")
         if err:
             await message.channel.send("チャンネル名を変更できませんでした。")
